315faa9aaf52bc978d1c5dbb1d458dd204f784e8
Henrique Ribeiro <pg38415@alunos.uminho.pt>
2020-01-07 18:55:28 +0000

    interface main now gets posts

--- Projeto/ISN/routes/index.js
+++ Projeto/ISN/routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var axios = require('axios');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/main', function(req, res, next) {
-  res.render('main', { title: 'Express' });
+  axios.get('http://localhost:3001/posts')
+    .then(dados => res.render('main', {posts:dados.data}))
+    .catch(e => res.render('erro',{erro: e}))
 });
 
 module.exports = router;
